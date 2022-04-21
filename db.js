3c40e60bfea4d857f0fab673d6000408fbbc7542
logan-wood <63706140+logan-wood@users.noreply.github.com>
2022-04-21 22:16:38 +1200

    views

--- db.js
+++ db.js
@@ -4,12 +4,22 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env'})
 
+
+try {
+  const tls = import('tls');
+} catch (err) {
+  console.log('tls support is disabled!');
+}
+
+
+
 //create connection
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER, 
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
+    
 });
 
 module.exports = db;
