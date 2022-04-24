105579ad55bf497eac2f43f3eb5c69937bdacd7d
logan-wood <63706140+logan-wood@users.noreply.github.com>
2022-04-24 19:28:18 +1200

    Update scribblenotes.sql

--- scribblenotes.sql
+++ scribblenotes.sql
@@ -13,18 +13,19 @@ SET time_zone = "+00:00";
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(16) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(16) NOT NULL,
-  PRIMARY KEY (`user_id`)
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+  user_id int NOT NULL AUTO_INCREMENT,
+  username VARCHAR(16) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(16) NOT NULL,
+  PRIMARY KEY (user_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS 'notes' (
-  'note_id' int NOT NULL AUTO_INCREMENT,
-  'csv_file' VARCHAR(255),
+CREATE TABLE IF NOT EXISTS notes (
+  note_id int NOT NULL AUTO_INCREMENT,
+  csv_file VARCHAR(255),
+  PRIMARY KEY (note_id)
 )
 
 COMMIT;
